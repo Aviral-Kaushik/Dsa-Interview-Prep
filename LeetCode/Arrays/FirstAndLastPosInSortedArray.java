858985f9c18e276927633c9dc946a48d814bf634
Aviral Kaushik <zaviralkaushik@gmail.com>
2025-07-28 14:42:13 +0530

    Solved Problems: Search in Rotated Sorted Array, First and Last Position of element in Sorted Array and Search Insert Position.

---
+++ LeetCode/Arrays/FirstAndLastPosInSortedArray.java
@@ -0,0 +1,58 @@
+package LeetCode.Arrays;
+
+public class FirstAndLastPosInSortedArray {
+
+    /**
+     * Efficient Method
+     * Time Complexity: O(log n)
+     * Space Complexity: O(1)
+     * */
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+
+            if (nums[mid] == target)
+                index = mid;
+        }
+
+        return index;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int index = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+            if (nums[mid] == target)
+                index = mid;
+        }
+
+        return index;
+    }
+}
---
+++ LeetCode/Arrays/SearchInRotatedSortedArray.java
@@ -0,0 +1,38 @@
+package LeetCode.Arrays;
+
+public class SearchInRotatedSortedArray {
+
+    /**
+     * Efficient Method
+     * Time Complexity: O(log n)
+     * Space Complexity: O(1)
+     * */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
