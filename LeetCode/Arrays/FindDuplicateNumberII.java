cefe69bf183e698a7003789a8ec419168329b878
Aviral Kaushik <zaviralkaushik@gmail.com>
2025-08-08 19:03:02 +0530

    Solved Problems: Find Duplicate Number II, Move Zeros and Nun Array.

---
+++ LeetCode/Arrays/FindDuplicateNumberII.java
@@ -0,0 +1,29 @@
+package LeetCode.Arrays;
+
+public class FindDuplicateNumberII {
+
+    /**
+     * Efficient Method
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     * */
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Phase 1: Find intersection point
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Phase 2: Find entrance to cycle
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
---
+++ LeetCode/Arrays/NumArray.java
@@ -0,0 +1,19 @@
+package LeetCode.Arrays;
+
+class NumArray {
+    private final int[] prefix;
+
+    public NumArray(int[] nums) {
+        prefix = new int[nums.length + 1]; // prefix[0] = 0
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+
+    /**
+     * Time Complexity: O(1)
+     * */
+    public int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+}
